d4ea8a9237effd0118e248352a505fa47022d7f3
eslessons <eslessons@tutanota.com>
2020-11-04 12:37:14 +0300

    Finish 2. Frontend Infrastructure

--- Complete Front-End Project - Build a Game.js
+++ Complete Front-End Project - Build a Game.js
@@ -8,6 +8,16 @@ Start
 Finish
 
 2. Frontend Infrastructure
+Start
+	1. Hello World
+	2. Build Process
+	3. Code Style
+	4. Code Formatting
+	5. Editor Setup
+	6. Linting
+	7. Testing & Type Checking
+Finish
+
 3. Architecture
 4. The Game
 5. States
