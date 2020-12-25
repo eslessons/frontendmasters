b331d7953b44b700bb17f9c56b511ae1eae6448e
ES Lessons <eslessons@tutanota.com>
2020-12-25 14:29:52 +0300

    Start Frontendmasters.com - Go for JavaScript Developers -> 1. Introduction

---
+++ Go for JavaScript Developers.js
@@ -0,0 +1,21 @@
+/* Frontendmaster.com 
+Go for JavaScript Developers
+
+1. Introduction
+Start
+
+2. Printing
+3. Basic Go Syntax
+4. Complex Structures
+5. Go Toolkit
+6. Structs
+7. Pointers
+8. Error Handling
+9. Methods
+10. Interfaces
+11. Web Servers
+12. Hitting an External API
+13. Concurrency
+14. Wrapping Up
+
+*/
