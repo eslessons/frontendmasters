2016f4f42899372c952edd236cc548ca84a911ea
ES Lessons <eslessons@tutanota.com>
2021-01-05 11:32:25 +0300

    Start Frontendmasters.com - Hardcore Functional Programming in JavaScript -> 1. Introduction

---
+++ Hardcore Functional Programming in JavaScript.js
@@ -0,0 +1,14 @@
+/* Frontendmasters.com 
+Hardcore Functional Programming in JavaScript, v2
+
+1. Introduction
+Start
+
+2. Currying
+3. Composition
+4. Functors
+5. Either Monad
+6. Task
+7. Wrapping Up
+
+*/
