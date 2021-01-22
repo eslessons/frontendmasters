b4e1430948d0996ab308c0bf81349d3608ce15e8
ES Lessons <eslessons@tutanota.com>
2021-01-22 12:05:19 +0300

    Start Frontendmasters.com - Introduction to the JAMStack -> 1. Introduction

---
+++ Introduction to the JAMStack.js
@@ -0,0 +1,14 @@
+/* Frontendmasters.com -
+Introduction to the JAMStack
+
+1. Introduction
+Start
+
+
+2. Vanilla JAMStack
+3. Serverless Contact Form
+4. User Dashboard SPA
+5. Persisting Data
+6. Wrapping Up
+
+*/
