201138df45f764e612ac74f8e30858499829fcdf
ES Lessons <eslessons@tutanota.com>
2021-01-25 11:21:23 +0300

    Start Frontendmasters.com - JavaScript - The Recent Parts -> 1. Introduction

---
+++ JavaScript - The Recent Parts.js
@@ -0,0 +1,18 @@
+/* Frontendmasters.com - 
+JavaScript - The Recent Parts
+
+1. Introduction
+Start
+
+
+2. Strings
+3. Array Destructuring
+4. Object Destructuring
+5. Further Destructuring
+6. Array Methods
+7. Iterators & Generators
+8. Regular Expressions
+9. Async Await
+10. Wrap-Up
+
+*/
