db4cd8ceaf08120999e364da70c861dc30b0845d
ES Lessons <eslessons@tutanota.com>
2021-02-01 11:33:43 +0300

    Start Frontendmasters.com - Modern Search Engine Optimization (SEO) -> 1. Introduction

---
+++ Modern Search Engine Optimization (SEO).js
@@ -0,0 +1,15 @@
+/* Frontendmasters.com - 
+Modern Search Engine Optimization (SEO)
+
+1. Modern SEO
+Start
+
+
+2. Traditional SEO Strategies
+3. Social Metadata
+4. Structured Data
+5. Mobile Optimizations
+6. Accelerated Mobile Pages (AMP) Project
+7. Wrapping Up Modern SEO
+
+*/
