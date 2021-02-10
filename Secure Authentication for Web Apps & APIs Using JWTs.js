e0d921f858f052ae1788437d8c8922d1916fa354
ES Lessons <eslessons@tutanota.com>
2021-02-10 11:19:25 +0300

    Start Frontendmasters.com - Secure Authentication for Web Apps & APIs Using JWTs -> 1. JSON Web Tokens

---
+++ Secure Authentication for Web Apps & APIs Using JWTs.js
@@ -0,0 +1,14 @@
+/* Frontendmasters.com - 
+Secure Authentication for Web Apps & APIs Using JWTs
+
+1. JSON Web Tokens
+Start
+
+
+2. JSON Web Tokens & Single Page Applications
+3. Client Side Sessions
+4. User Information in the Payload
+5. Protecting Resources
+6. Protecting Routes
+
+*/
