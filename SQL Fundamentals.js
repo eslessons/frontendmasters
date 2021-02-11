8ff390451bacd0816dc95a9bbd87b07116079590
ES Lessons <eslessons@tutanota.com>
2021-02-11 09:15:23 +0300

    Start Frontendmasters.com - SQL Fundamentals -> 1. Foundations of Relational Databases

---
+++ SQL Fundamentals.js
@@ -0,0 +1,13 @@
+/* Frontendmasters.com -
+SQL Fundamentals
+
+1. Foundations of Relational Databases
+Start
+
+2. Retrieving Data
+3. Querying Across Tables
+4. Creating, Updating, and Deleting
+5. The Schema Evolves
+6. Wrapping Up SQL Fundamentals
+
+*/
