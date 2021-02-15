d90d1f568f44b9307d76212d31b838f8426b911f
ES Lessons <eslessons@tutanota.com>
2021-02-15 08:52:26 +0300

    Start Frontendmasters.com - Enterprise Architecture Patterns -> 1. Introduction

---
+++ Enterprise Architecture Patterns.js
@@ -0,0 +1,16 @@
+/* Frontendmasters.com - 
+Enterprise Architecture Patterns
+
+1. Introduction
+Start
+
+
+2. Complexity
+3. Object Oriented Programming
+4. Decisions & Conditionals
+5. Collections & Iterators
+6. Time Management in Applications
+7. Distributed Complexity
+8. Wrapping Up
+
+*/
