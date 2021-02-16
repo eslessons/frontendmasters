fa167abe6a8021e5f2b1c097bd7b5328226a9e89
ES Lessons <eslessons@tutanota.com>
2021-02-16 10:09:22 +0300

    Finish 1. Introduction

---
+++ Interviewing for Front-End Engineers.js
@@ -0,0 +1,16 @@
+/* Frontendmasters.com -
+Interviewing for Front-End Engineers
+
+1. Introduction
+Start
+Finish
+
+2. Application
+3. Initial Call
+4. Code Test
+5. Phone Screen
+6. On-site Interview & Concept Review
+7. On-site Interview Example Questions
+8. Wrapping Up
+
+*/
