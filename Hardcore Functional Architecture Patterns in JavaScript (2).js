6ed3657cb69c82cf824026cc097c8c3fe94b1eb0
ES Lessons <eslessons@tutanota.com>
2021-02-16 09:50:16 +0300

    Start Frontendmasters.com - Hardcore Functional Architecture Patterns in JavaScript -> 1. Introduction

---
+++ Hardcore Functional Architecture Patterns in JavaScript (2).js
@@ -0,0 +1,13 @@
+/* Frontendmasters.com - 
+Hardcore Functional Architecture Patterns in JavaScript
+
+1. Introduction
+Start
+
+
+2. Semigroups & Monoids
+3. Function Modeling
+4. Monadic Web Apps
+5. Wrapping Up
+
+*/
