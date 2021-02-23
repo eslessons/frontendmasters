1852a42d064d1b90469169799566399005e7f1ed
ES Lessons <eslessons@tutanota.com>
2021-02-23 10:07:46 +0300

    Start Frontendmasters.com - Webpack - Webpack 4 Fundamentals -> 1. Why Webpack

---
+++ Webpack.js
@@ -0,0 +1,24 @@
+/* Frontendmasters.com - 
+Webpack - Webpack 4 Fundamentals (Part I)
+
+1. Why Webpack
+Start
+
+
+2. Webpack from Scratch
+3. Webpack Core Concepts
+4. Using Plugins
+5. Wrapping Up
+
+Webpack - Mastering Webpack Plugins (Part II)
+
+1. Plugin System
+2. Creating Plugins
+3. Config, Loaders & Babel
+
+Webpack - Web Performance with Webpack (Part III)
+
+1. Web Performance
+2. Code Splitting
+3. Module Methods & Magic Comments
+
