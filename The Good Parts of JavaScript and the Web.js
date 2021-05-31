83f990c73a1cfea7cfb1b60c33f3862bac0d1c88
ES Lessons <eslessons@tutanota.com>
2021-05-31 12:06:58 +0300

    Start The Good Parts of JavaScript and the Web

---
+++ The Good Parts of JavaScript and the Web.js
@@ -0,0 +1,14 @@
+/* Frontendmasters.com - 
+The Good Parts of JavaScript and the Web 
+
+1. Programming Style and your Brain
+2. And Then There Was JavaScript 
+3. Function the Ultimate    
+4. The Metamorphosis of Ajax
+5. ES5 - The New Parts
+6. Fun with Functions
+7. Principles of Security
+8. Managing Asynchronicity
+9. The Better Parts
+
+*/
